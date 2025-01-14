9d4d19e52c9630cfc6e04cec5924a186c3214516
DavidIncarbone <david.incarbone@gmail.com>
2025-01-14 12:09:27 +0100

    add sections on main and card components

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+
+    return (
+        <div id="card" className="debug ">
+
+            <img src="https://picsum.photos/id/237/200/300" alt="random" />
+
+
+        </div>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,34 @@
+import Card from "./Card"
+
 function Main() {
     return (
-        <main>
+        <main className="mt-5">
+            <section id="movies" className="container">
+
+                <h2>Movies</h2>
+
+                <div className="d-flex container debug">
+                    <div className="row col-3 p-3">
+
+                        <Card />
+
+                    </div>
+                </div>
+
+            </section>
+
+            <section id="TV-series" className="container">
+                <h2>TV Series</h2>
+
+                <div className="d-flex container debug">
+                    <div className="row col-3 p-3">
+
+                        <Card />
+
+                    </div>
+                </div>
+
+            </section>
 
 
         </main>
